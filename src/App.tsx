8fde00a030c5318163fdb19827fec6ed2f50f3e2
Vadym Danyliuk <vadym.danyliuk.ua@gmail.com>
2024-06-14 07:13:28 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { initClient, ClientProvider } from "@swapcard/react-sdk/lib/client";
+import { ApplicationProvider } from "@swapcard/react-sdk/lib/application";
+import { EventProvider } from "@swapcard/react-sdk/lib/event";
+import { ExhibitorEventListView } from "@swapcard/react-sdk/lib/exhibitor/event-list-view";
+
+export default function App() {
+    const client = initClient();
+
+    console.log(import.meta.env.VITE_PUBLIC_EVENT_ID);
+    console.log(import.meta.env.VITE_PUBLIC_EXHIBITOR_VIEW_ID);
+
+    return (
+        <ClientProvider client={client}>
+            <ApplicationProvider>
+                <EventProvider eventId={import.meta.env.VITE_PUBLIC_EVENT_ID}>
+                    <ExhibitorEventListView viewId={import.meta.env.VITE_PUBLIC_EXHIBITOR_VIEW_ID} />
+                </EventProvider>
+            </ApplicationProvider>
+        </ClientProvider>
+    );
+}
---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+declare module '@swapcard/react-sdk/lib/client';
+declare module '@swapcard/react-sdk/lib/application';
+declare module '@swapcard/react-sdk/lib/event';
+declare module '@swapcard/react-sdk/lib/exhibitor/event-list-view';
